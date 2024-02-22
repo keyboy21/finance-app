7c5b2e1935bf0d1759c81cac0bc8394d041c42fe
Yusuf <yusufsaydullayev@gmail.com>
2024-02-22 15:10:16 +0500

    improve chunking

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,21 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id
+							.toString()
+							.split('node_modules/')[1]
+							.split('/')[0]
+							.toString();
+					}
+				},
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			'~': path.resolve(__dirname, './src'),
